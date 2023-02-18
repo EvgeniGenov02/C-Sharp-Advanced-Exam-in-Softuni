3a5ddfc161b083689d7d1fce01e666bef12b42f6
Evgeni Genov <44731191+EvgeniGenov02@users.noreply.github.com>
2023-02-18 20:00:55 +0200

    03. SoftUni Kindergarten

---
+++ 03. SoftUni Kindergarten/Child.cs
@@ -0,0 +1,53 @@
+﻿namespace SoftUniKindergarten
+{
+    public class Child
+    {
+
+        
+        private string firstName;
+        private string contactNumber;
+        private int age;
+        private string lastName;
+        private string parentName;
+        //firstName, lastName, age, parentName, contactNumber.
+        public Child(string firstName,string lastName, int age, string parentName, string contactNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ContactNumber = contactNumber;
+            Age = age;
+            ParentName = parentName;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string ParentName
+        {
+            get { return parentName; }
+            set { parentName = value; }
+        }
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set { contactNumber = value; }
+        }
+
+        public override string ToString() 
+        {
+      return $"Child: {FirstName} {LastName}, Age: {Age}, Contact info: {ParentName} - {ContactNumber}";
+        }
+    }
+}
---
+++ 03. SoftUni Kindergarten/Kindergarten.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+		//Name - string
+		//Capacity - int 
+		//Registry - List<Child>
+
+		private string name;
+		private int capacity;
+        private List<Child> childrens;
+
+		public Kindergarten(string name , int capacity)
+		{
+			Name= name;
+			Capacity = capacity;
+            childrens = new List<Child>();
+
+        }
+
+        public string Name
+        {
+			get { return name; }
+			set { name = value; }
+		}
+		public int Capacity
+        {
+			get { return capacity; }
+			set { capacity = value; }
+		}
+
+       public bool AddChild(Child child)
+		{
+			if (childrens.Count < Capacity)
+			{
+			childrens.Add(child);
+			return true;
+			}
+			return false;
+		}
+       public bool RemoveChild(string childFullName)
+		{
+				string[] nameTokens = childFullName.Split(" ",System.StringSplitOptions.RemoveEmptyEntries);
+                string firstName = nameTokens[0];
+				string familName = nameTokens[1];
+			foreach (var children in childrens)
+			{
+				if (children.FirstName == firstName && children.LastName == familName)
+				{ 
+                return true; 
+				}
+			}
+				
+			return false;
+        }
+
+	  public int ChildrenCount { get { return childrens.Count; } }
+
+       public Child GetChild(string childFullName)
+		{
+            string[] nameTokens = childFullName.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string firstName = nameTokens[0];
+            string familName = nameTokens[1];
+            foreach (var children in childrens)
+            {
+                if (children.FirstName == firstName && children.LastName == familName)
+                {
+                    return children;
+                }
+            }
+            return null;
+		}
+
+        public string RegistryReport()
+		{
+			if (childrens != null)
+			{
+				childrens = childrens.OrderByDescending(c => c.Age)
+					.ThenBy(c => c.LastName)
+					.ThenBy(c => c.FirstName)
+					.ToList();
+				StringBuilder sb = new StringBuilder();
+				sb.AppendLine($"Registered children in {Name}:");
+
+				foreach (var child in childrens)
+				{
+					sb.AppendLine(child.ToString());
+				}
+			return sb.ToString().Trim();
+			}
+			return null;
+        }
+    }
+}
